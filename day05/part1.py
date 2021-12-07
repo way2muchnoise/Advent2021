b1b1bfd83a0d5f29c69e2a0c8ec356c6d8b00414
way2muchnoise <jens.baetens@live.be>
2021-12-07 18:16:47 +0100

    check-in day05

---
+++ day05/part1.py
@@ -0,0 +1,32 @@
+import re
+
+input_pattern = re.compile(r'(\d+),(\d+) -> (\d+),(\d+)')
+grid = [[0 for i in range(1000)] for j in range(1000)]
+
+
+def draw_line(points):
+    global grid
+    x1, y1, x2, y2 = points
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    if x1 == x2:
+        for i in range(min(y1, y2), max(y1, y2) + 1):
+            grid[x1][i] += 1
+    elif y1 == y2:
+        for i in range(min(x1, x2), max(x1, x2) + 1):
+            grid[i][y1] += 1
+
+
+with open('input.txt', 'r') as f:
+    line = f.readline()
+    while line is not None and line != '':
+        draw_line(input_pattern.match(line).groups())
+        line = f.readline()
+
+dangerous_cells = 0
+
+for row in grid:
+    for cell in row:
+        if cell >= 2:
+            dangerous_cells += 1
+
+print(dangerous_cells)
